e941566a6d83dbea49efc075286416d58e3af7e1
tanjimin <tanjimin@hotmail.com>
2023-06-02 19:15:10 +0000

    train script

--- src/model/run.sh
+++ src/model/run.sh
@@ -5,18 +5,11 @@ NUM_NODES=1
 GPUS_PER_NODE=2
 
 # Define the command to run on each node
-CMD = "python main_dist.py " \
-      "--split tf_in_domain " \
-      "--run_dir /home/ubuntu/codebase/tf_binding/runs/ " \
-      "--epoches 200 " \
-      "--batch_size 128"
+CMD="main_dist.py --split tf_and_chr --run_dir /home/ubuntu/codebase/tf_binding/runs/ --epoches 200 --batch_size 128"
 # Define the master node
-MASTER_NODE=0
 
 # Define the port number
 PORT=1234
 
 # Launch the distributed training
-torchrun --nnodes=$NUM_NODES --nproc_per_node=$GPUS_PER_NODE \
-    --node_rank=$MASTER_NODE --master_addr="node-${MASTER_NODE}" \
-    --master_port=$PORT $CMD
+torchrun --nproc_per_node=$GPUS_PER_NODE $CMD
